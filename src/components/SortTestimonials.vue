daf6826ef9a19141ad69d7ce7ad9e875a35fe1f8
CCotuna <calin.cotuna@emanuel.ro>
2024-01-31 22:50:12 +0200

    Implementing logic for filtering the testimonials cards

---
+++ src/components/SortTestimonials.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+defineEmits(['topReviews', 'topLikes'])
+
+defineProps({
+  sortSens: String,
+  sortActive: Number,
+  required: true
+})
+</script>
+
+<template>
+  <div>
+    Sort By:
+    <div>
+      <button @click="$emit('topReviews')">Reviews
+        <span v-if="sortActive === 1">
+          <i v-if="sortSens === 'ASC'" class="bi bi-arrow-up-right"></i>
+        <i v-else class="bi bi-arrow-down-left"></i></span>
+    </button>
+        
+      <button @click="$emit('topLikes')">Likes
+        <span v-if="sortActive === 2">
+          <i v-if="sortSens === 'ASC'" class="bi bi-arrow-up-right"></i>
+        <i v-else class="bi bi-arrow-down-left"></i></span>
+    </button>
+    </div>
+  </div>
+</template>
--- src/views/TestimonialsPage.vue
+++ src/views/TestimonialsPage.vue
@@ -1,9 +1,47 @@
 <script setup>
 import TestimonialCard from '../components/TestimonialCard.vue';
+import SortTestimonials from '../components/SortTestimonials.vue';
 
 import testimonialData from '../data/testimonialsData.json';
+
+import { ref } from 'vue';
+
+const sortSens = ref("ASC");
+const sortActive = ref(1);
+
+const sortedTestimonials = ref(testimonialData);
+
+const sortTopReviews = () => {
+   if(sortSens.value === "ASC"){
+    sortedTestimonials.value.sort((a,b) => a.reviews - b.reviews);
+    sortSens.value = "DESC";
+   } else {
+    sortedTestimonials.value.sort((a,b) => b.reviews - a.reviews);
+    sortSens.value = "ASC";
+   }
+   sortActive.value = 1;
+}
+
+const sortTopLikes = () => {
+   if(sortSens.value === "ASC"){
+    sortedTestimonials.value.sort((a,b) => a.likes - b.likes);
+    sortSens.value = "DESC";
+   } else {
+    sortedTestimonials.value.sort((a,b) => b.likes - a.likes);
+    sortSens.value = "ASC";
+   }
+   sortActive.value = 2;
+}
+
+import { onMounted } from 'vue'
+onMounted(() => {
+  sortTopReviews();
+})
+
 </script>
 <template>
+
+    <SortTestimonials :sortSens="sortSens" :sortActive="sortActive" @topReviews="sortTopReviews" @topLikes="sortTopLikes"/>
     <div class="flex flex-col px-24 py-10">
         <div class="flex flex-wrap -mx-3 mb-3">
             <TestimonialCard v-for="testimonial in testimonialData" :key="testimonial.title" :testimonial="testimonial" />
