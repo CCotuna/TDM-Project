8c97a3d61cbb5e4d5c035ccf1b90c8b512bdbb49
CCotuna <calin.cotuna@emanuel.ro>
2024-01-31 17:11:40 +0200

    Worked on Testimonial Card

--- src/views/TestimonialsPage.vue
+++ src/views/TestimonialsPage.vue
@@ -1,3 +1,13 @@
-<script setup></script>
-<template></template>
+<script setup>
+import TestimonialCard from '../components/TestimonialCard.vue';
+
+import testimonialData from '../data/testimonialsData.json';
+</script>
+<template>
+    <div class="flex flex-col px-24 py-10">
+        <div class="flex flex-wrap -mx-3 mb-3">
+            <TestimonialCard v-for="testimonial in testimonialData" :key="testimonial.title" :testimonial="testimonial" />
+        </div>
+    </div>
+</template>
 <style scoped></style>
