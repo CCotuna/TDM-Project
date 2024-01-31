9f189c4f94cef1a693fd06fa473087fb2bbdf93b
CCotuna <calin.cotuna@emanuel.ro>
2024-01-31 12:04:17 +0200

    Initial Docker Setup and Project Structure for Vue App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <!-- <div class="bg-white"></div> -->
-  <NavBar />
+  <div class="flex flex-col min-h-screen">
+    <main class="flex-1">
+      <NavBar />
+    </main>
+    <Footer />  
+  </div>
+
+  
 </template>
 
 <style scoped>
