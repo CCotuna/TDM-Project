7658632c180307b2b9ac3cfedffe6094df345789
CCotuna <calin.cotuna@emanuel.ro>
2024-01-31 15:14:24 +0200

    Updated routes - because I forgot to change the import, it displayed continuosly the HomePage component

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,8 +1,15 @@
 <script setup>
 import AboutImage from '../components/AboutImage.vue'
+import AboutCallToAction from '../components/AboutCallToAction.vue'
 </script>
 <template>
-    <AboutImage src="https://picsum.photos/200/300" />
-    this is the about page
+
+<div class="bg-white">
+    <div class="grid grid-cols-1 lg:grid-cols-2 relative isolate px-6 pt-10">
+        <AboutImage src="https://picsum.photos/200/200" />
+        <AboutCallToAction />
+    </div>
+</div>
+    
 </template>
 <style scoped></style>
