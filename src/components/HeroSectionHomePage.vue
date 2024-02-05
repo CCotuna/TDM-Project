463b744c14d682ed12d9eb21a27a02d224547500
CCotuna <calin.cotuna@emanuel.ro>
2024-02-05 14:06:21 +0200

    Working on design on figma - coming soon - New Homepage

--- src/components/HeroSectionHomePage.vue
+++ src/components/HeroSectionHomePage.vue
@@ -1,8 +1,30 @@
-<script setup></script>
+<script setup>
+
+import { onMounted } from 'vue';
+
+onMounted(() => {
+});
+
+// Import the JSON animation file directly
+import animationData from '../assets/animation2.json';
+
+// Define Lottie animation options
+const lottieOptions = {
+  animationData: animationData,
+  loop: true,
+  autoplay: true,
+};
+</script>
 <template>
-    <div class="py-20 text-start">
+    <div class="py-20 text-center">
         <h1 class="text-6xl font-bold mb-8 text-white">Welcome to Our <br> Photography World</h1>
         <p class="text-lg text-gray-200">Capturing Moments, Creating Memories, and Preserving Emotions with Every Click</p>
+        <!-- <Vue3Lottie
+    :animationData="lottieOptions.animationData"
+    :loop="lottieOptions.loop"
+    :autoplay="lottieOptions.autoplay"
+    :width="200"
+  /> -->
     </div>
 </template>
 <style scoped>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,34 +3,13 @@ import HomePageCard from '../components/HomePageCard.vue';
 import HeroSectionHomePage from '../components/HeroSectionHomePage.vue';
 import heroSectionHomePageData from '../data/heroSectionHomePageData.json';
 
-import { Vue3Lottie } from 'vue3-lottie'
-
-import { ref, onMounted } from 'vue';
+import { ref } from 'vue';
 const heroSectionData = ref(heroSectionHomePageData);
-
-onMounted(() => {
-});
-
-// Import the JSON animation file directly
-import animationData from '../assets/animation2.json';
-
-// Define Lottie animation options
-const lottieOptions = {
-  animationData: animationData,
-  loop: true,
-  autoplay: true,
-};
 
 </script>
 <template>
-  <HeroSectionHomePage />
-  <div class="">
-    <Vue3Lottie
-    :animationData="lottieOptions.animationData"
-    :loop="lottieOptions.loop"
-    :autoplay="lottieOptions.autoplay"
-    :width="200"
-  />
+  <div class="flex items-start justify-center flex-col">
+    <HeroSectionHomePage />
   </div>
 
 </template>
