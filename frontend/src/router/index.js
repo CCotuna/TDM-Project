c39445aebc895fed54c47e437d6fbe1a38694122
CCotuna <calin.cotuna@emanuel.ro>
2024-04-18 10:31:25 +0300

    Created initial routes setup for contact form, login, logout and testimonials

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,56 +1,56 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+  import { createRouter, createWebHistory } from 'vue-router'
+  import HomeView from '../views/HomeView.vue'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/index',
-      name: 'homepage',
-      component: HomeView
-    }, {
-      path: '/',
-      name: 'homepage',
-      component: HomeView
-    },
-    {
-      path: '/About',
-      name: 'about',
-      component: () => import('@/views/AboutView.vue'),
-    },
-    {
-      path: '/Services',
-      name: 'services',
-      component: () => import('@/views/ServicesView.vue'),
-    },
-    {
-      path: '/Portfolio',
-      name: 'portfolio',
-      component: () => import('@/views/ServicesView.vue'),
-    },
-    {
-      path: '/Contact',
-      name: 'contact',
-      component: () => import('@/views/ContactView.vue'),
-    },
-    {
-      path: '/Galleries',
-      name: 'galleries',
-      component: () => import('@/views/GalleriesView.vue'),
-    },
-    {
-      path: '/Portfolio',
-      name: 'portfolio',
-      component: () => import('@/views/PortfolioView.vue'),
+  const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+      {
+        path: '/index',
+        name: 'homepage',
+        component: HomeView
+      }, {
+        path: '/',
+        name: 'homepage',
+        component: HomeView
+      },
+      {
+        path: '/About',
+        name: 'about',
+        component: () => import('@/views/AboutView.vue'),
+      },
+      {
+        path: '/Services',
+        name: 'services',
+        component: () => import('@/views/ServicesView.vue'),
+      },
+      {
+        path: '/Portfolio',
+        name: 'portfolio',
+        component: () => import('@/views/ServicesView.vue'),
+      },
+      {
+        path: '/Contact',
+        name: 'contact',
+        component: () => import('@/views/ContactView.vue'),
+      },
+      {
+        path: '/Galleries',
+        name: 'galleries',
+        component: () => import('@/views/GalleriesView.vue'),
+      },
+      {
+        path: '/Portfolio',
+        name: 'portfolio',
+        component: () => import('@/views/PortfolioView.vue'),
+      }
+    ],
+    scrollBehavior(savedPosition) {
+      if(savedPosition) {
+        return savedPosition
+      } else {
+        return { top: 0 } 
+      }
     }
-  ],
-  scrollBehavior(savedPosition) {
-    if(savedPosition) {
-      return savedPosition
-    } else {
-      return { top: 0 } 
-    }
-  }
-})
+  })
 
-export default router
+  export default router
