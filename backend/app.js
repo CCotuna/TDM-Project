4c2b4bf79600b1a0efcb7301e7671f27a1ddf77f
CCotuna <calin.cotuna@emanuel.ro>
2024-04-18 10:26:46 +0300

    Added CORS middleware to allow cross-origin requests and set up server to listen on port 3000

--- backend/app.js
+++ backend/app.js
@@ -1,57 +1,25 @@
-import http from "http";
-import { parse } from "url";
+import express from "express";
+import bodyParser from "body-parser";
 
-const hostname = "0.0.0.0";
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  const headers = {
-    "Access-Control-Allow-Origin": "http://localhost:5173", // Replace with the origin of your frontend application
-    "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    "Access-Control-Allow-Credentials": "true",
-  };
+app.use(bodyParser.json());
 
-  const { pathname, query } = parse(req.url, true);
-
-  if (req.method === "OPTIONS") {
-    // Preflight request handling
-    res.writeHead(204, headers);
-    res.end();
-  } else if (pathname === "/login" && req.method === "POST") {
-    // Handle login request
-    // Your login logic goes here
-  } else {
-    // Handle other requests
-    res.writeHead(200, headers);
-    res.setHeader("Content-Type", "application/json");
-    res.end(
-      JSON.stringify([
-        {
-          id: "e03fa61c-bab5-46d2-bba3-9e040dc671f6",
-          name: "abceddddddddd2323",
-          favorite: true,
-        },
-        {
-          id: "afac3455-1394-4682-ab12-19ba2bd523e8",
-          name: "task 3",
-          favorite: true,
-        },
-        {
-          id: "afac3455-1394-4682-ab12-19ba2bd456e8",
-          name: "new task",
-          favorite: true,
-        },
-        {
-          id: "afac3455-1394-4682-ab12-19ba2bd123e8",
-          name: "taskkkkkk",
-          favorite: true,
-        },
-      ])
-    );
-  }
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
