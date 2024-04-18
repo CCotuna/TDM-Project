4896b792e6b8bb8f8807a8d0e6ff37ee4615b7b8
CCotuna <calin.cotuna@emanuel.ro>
2024-04-19 02:21:33 +0300

    Working later on dashboard :)

--- dashboard/src/views/HomeView.vue
+++ dashboard/src/views/HomeView.vue
@@ -1,9 +1,3 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<script setup></script>
+<template>asd</template>
+<style scoped></style>
--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -5,15 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  server: {
+    port: 5180,
+    fs: {
+      allow: ['..']
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  server: {
-    port: 5180
   }
 })
