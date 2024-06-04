0be26b27785713cc851585caf67872cfc7b42573
CCotuna <calin.cotuna@emanuel.ro>
2024-06-04 15:04:18 +0300

    WS for chat - testing

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import "./wsServer.js";
+
 const app = express();
 const port = 3000;
 
---
+++ backend/wsServer.js
@@ -0,0 +1,30 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+const clients = [];
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  clients.push(ws);
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+
+    clients.forEach((wsClient) => {
+      if (ws !== wsClient) {
+        wsClient.send(data.toString());
+      }
+    });
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+
+  ws.onerror = function () {
+    console.log("Some Error occurred");
+  };
+});
+
+export default wss;
